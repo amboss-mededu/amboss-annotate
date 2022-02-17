108a9f20836da252338575fd9386a3c9c38a32c4
ajt <alexjeffcott@gmail.com>
2022-02-17 14:16:17 +0100

    Check custom elements don't exist before creating them

--- src/index.js
+++ src/index.js
@@ -3,24 +3,28 @@ import { AmbossContentCard } from '@amboss-mededu/amboss-phrasio'
 import AmbossAnchor from "./anchor-custom-element";
 
 function initAnnotation() {
-  window.ambossAnnotationAdaptor = {}
-  window.ambossAnnotationOptions = {}
+  if (!window.ambossAnnotationAdaptor) window.ambossAnnotationAdaptor = {}
+  if (!window.ambossAnnotationOptions) window.ambossAnnotationOptions = {}
+  if (!window.ambossAnnotationOptions.Content) {
+    window.ambossAnnotationOptions = {
+      Content: AmbossContentCard,
+      Anchor: AmbossAnchor,
+      shouldAnnotate: true,
+      token: '',
+      offline: true,
+      campaign: '',
+      locale: 'unset',
+      annotationVariant: 'underline',
+      theme: 'light-theme',
+      customBranding: 'no',
+    };
+  }
 
-  window.ambossAnnotationOptions = {
-    Content: AmbossContentCard,
-    Anchor: AmbossAnchor,
-    shouldAnnotate: true,
-    token: '',
-    offline: true,
-    campaign: '123890',
-    locale: 'us',
-    annotationVariant: 'underline',
-    theme: 'light-theme',
-    customBranding: 'no',
-  };
+  if(window.customElements.get('amboss-content-card') === undefined)
+    window.customElements.define('amboss-content-card', AmbossContentCard)
 
-  window.customElements.define('amboss-content-card', AmbossContentCard)
-  window.customElements.define('amboss-anchor', AmbossAnchor)
+  if(window.customElements.get('amboss-anchor') === undefined)
+    window.customElements.define('amboss-anchor', AmbossAnchor)
 }
 
 export { annotate, initAnnotation, getPhrasiosFromText };
